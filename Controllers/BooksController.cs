d8eb3784cece08b52be05a1ddef01b08ee7b908d
Jose Mª Izquierdo Galiot <igaliot21@gmail.com>
2020-08-05 13:03:29 +0200

    Add project files.

---
+++ Controllers/BooksController.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookList.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookList.Controllers
+{
+    public class BooksController : Controller
+    {
+        private readonly AppDbContext context;
+        
+        public BooksController(AppDbContext Context)
+        {
+            this.context = Context;
+        }
+
+        public IActionResult Index(){
+            return View(context.Books.ToList());
+        }
+
+        //Get : Book/Create
+        public IActionResult Create() {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Book book){
+            if (ModelState.IsValid) {
+                context.Add(book);
+                await context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(book);
+        }
+
+        //Details : Books/Details/5 <- id
+        public async Task<IActionResult> Details(int? id) {
+            if (id == null) {
+                return NotFound();
+            }
+            else {
+                var book = await context.Books.SingleOrDefaultAsync(b => b.Id == id);
+                if (book == null)
+                {
+                    return NotFound();
+                }
+                else {
+                    return View(book);
+                }
+            }
+        }
+        
+        //Edit : Books/Edit/5 <- id
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                var book = await context.Books.SingleOrDefaultAsync(b => b.Id == id);
+                if (book == null)
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    return View(book);
+                }
+            }
+        }
+        //POST: Books/Edit/5 <- id
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int? id, Book book) {
+            if (id != book.Id)
+            {
+                return NotFound();
+            }
+            else if (ModelState.IsValid)
+            {
+                context.Books.Update(book);
+                await context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(book);
+        }
+
+        //Delete : Books/Delete/5 <- id
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                var book = await context.Books.SingleOrDefaultAsync(b => b.Id == id);
+                if (book == null)
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    return View(book);
+                }
+            }
+        }
+
+        //POST: Books/Delete/5 <- id
+        [HttpPost,ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> RemoveBook(int? id)
+        {
+
+            if (id == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                var book = await context.Books.SingleOrDefaultAsync(b => b.Id == id);
+                if (book == null){
+                    return NotFound();
+                }
+                else{
+                    context.Books.Remove(book);
+                    await context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+        }
+
+        protected override void Dispose(bool disposing){
+            if (disposing) context.Dispose();
+        }
+    }
+}
---
+++ Migrations/20200805094841_AddNameMaxLength.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BookList.Migrations
+{
+    public partial class AddNameMaxLength : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Books",
+                maxLength: 512,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Books",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 512);
+        }
+    }
+}
